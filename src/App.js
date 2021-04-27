64551eab28fb1fe575dc83a47de1a041c2cbb007
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-04-27 14:39:00 +0200

    adds keys

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import NavBar from "./components/NavBar/NavBar";
-import Home from "./pages/home/Home";
-import NewStory from "./pages/new-story/NewStory";
-import Topics from "./pages/topics/Topics";
-import Read from "./pages/read/Read"
-import Search from "./pages/search/Search"
-import Stats from "./pages/stats"
-import Stories from "./pages/stories"
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/home/Home';
+import NewStory from './pages/new-story/NewStory';
+import Topics from './pages/topics/Topics';
+import Read from './pages/read/Read';
+import Search from './pages/search/Search';
+import Stats from './pages/stats';
+import Stories from './pages/stories';
+import { v4 as uuidv4 } from 'uuid';
 const routes = [
-  { path: "/", component: Home },
-  { path: "/new-story", component: NewStory },
-  { path: "/topics", component: Topics },
-  { path: "/read/:slug", component: Read },
-  { path: "/search", component: Search },
-  { path: "/stats", component: Stats },
-  { path: "/stories", component: Stories },
-]
+  { path: '/', component: Home },
+  { path: '/new-story', component: NewStory },
+  { path: '/topics', component: Topics },
+  { path: '/read/:slug', component: Read },
+  { path: '/search', component: Search },
+  { path: '/stats', component: Stats },
+  { path: '/stories', component: Stories },
+];
 
 function App() {
   return (
     <Router>
       <NavBar />
-      {
-        routes.map(({ path, component }) =>
-          <Route exact path={path} component={component} />
-        )
-      }
+      {routes.map(({ path, component }) => (
+        <Route key={uuidv4()} exact path={path} component={component} />
+      ))}
     </Router>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { IoBookmarksOutline } from "react-icons/io5";
-import ArticleListItem from "../../components/ArticleListItem/ArticleListItem";
-import Footer from "../../components/Footer/Footer";
-import PeopleList from "../../components/PeopleList/PeopleList";
-import TopicsToFollow from "../../components/TopicsToFollow/TopicsToFollow";
-import articles from "./articles.json";
-import "./styles.scss";
-
+import React, { Component } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { IoBookmarksOutline } from 'react-icons/io5';
+import ArticleListItem from '../../components/ArticleListItem/ArticleListItem';
+import Footer from '../../components/Footer/Footer';
+import PeopleList from '../../components/PeopleList/PeopleList';
+import TopicsToFollow from '../../components/TopicsToFollow/TopicsToFollow';
+import articles from './articles.json';
+import './styles.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Home extends Component {
   state = {
@@ -18,23 +18,24 @@ export default class Home extends Component {
       <div>
         <Container>
           <Row
-            className={"row-cols-lg-3 pb-4"}
-            style={{ borderBottom: "1px solid rgba(230, 230, 230, 1)" }}
+            className={'row-cols-lg-3 pb-4'}
+            style={{ borderBottom: '1px solid rgba(230, 230, 230, 1)' }}
           >
             <Col>
               <ArticleListItem
                 article={this.state.articles[0]}
-                articleImg={"top"}
-                headingFont={"large"}
+                articleImg={'top'}
+                headingFont={'large'}
                 subheading
               />
             </Col>
 
-            <Col className={"flex-column w-100"}>
+            <Col className={'flex-column w-100'}>
               {this.state.articles.slice(1, 5).map((article) => (
                 <ArticleListItem
-                  articleImg={"left"}
-                  headingFont={"small"}
+                  key={uuidv4()}
+                  articleImg={'left'}
+                  headingFont={'small'}
                   article={article}
                 />
               ))}
@@ -42,33 +43,38 @@ export default class Home extends Component {
 
             <Col>
               <PeopleList />
-              <TopicsToFollow/>
+              <TopicsToFollow />
             </Col>
-            <Col className={""}>{/*<TagsList />*/}</Col>
+            <Col className={''}>{/*<TagsList />*/}</Col>
           </Row>
-          <Row className={"py-4 mt-4"}>
-            <Col className={"col-lg-8 pr-5 pl-2"}>
+          <Row className={'py-4 mt-4'}>
+            <Col className={'col-lg-8 pr-5 pl-2'}>
               {this.state.articles.slice(6).map((article) => (
                 <ArticleListItem
-                  articleImg={"left"}
-                  headingFont={"large"}
+                  key={uuidv4()}
+                  articleImg={'left'}
+                  headingFont={'large'}
                   subheading
                   article={article}
                 />
               ))}
             </Col>
-            <Col className={"col-lg-4 "}>
+            <Col className={'col-lg-4 '}>
               <div
-                className={"flex-column py-4 px-4 w-100"}
-                style={{ backgroundColor: "rgb(250, 250, 250)" }}
+                className={'flex-column py-4 px-4 w-100'}
+                style={{ backgroundColor: 'rgb(250, 250, 250)' }}
               >
-                <div className={"mb-4 title"}>
-                  {" "}
-                  <IoBookmarksOutline style={{ fontSize: 20 }} />{" "}
-                  <span className={"ml-2"}>READING LIST </span>
+                <div className={'mb-4 title'}>
+                  {' '}
+                  <IoBookmarksOutline style={{ fontSize: 20 }} />{' '}
+                  <span className={'ml-2'}>READING LIST </span>
                 </div>
                 {this.state.articles.slice(0, 3).map((article) => (
-                  <ArticleListItem headingFont={"small"} article={article} />
+                  <ArticleListItem
+                    key={uuidv4()}
+                    headingFont={'small'}
+                    article={article}
+                  />
                 ))}
               </div>
               <Footer />
